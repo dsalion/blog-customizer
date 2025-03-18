30d8d88ff990d7d89980116b0b5726482559cd5c
Мамаев Евгений <ixsinu@santur.ru>
2025-03-18 14:06:59 +0500

    added openHandler, added 2 states

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,40 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useState } from 'react';
+import { Text } from 'src/ui/text';
+import { fontFamilyOptions, OptionType } from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const handlerOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const [selectedFontFamily, setSelectedFontFamily] = useState(
+		fontFamilyOptions[0]
+	);
+	const handleFontFamilyChange = (option: OptionType) => {
+		setSelectedFontFamily(option);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={handlerOpen} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						selected={selectedFontFamily}
+						onChange={handleFontFamilyChange}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
